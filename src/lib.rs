e8d45c8253287460817fbaf312917941f9f756f4
Aoi1011 <ht.so.ik0102@gmail.com>
2023-03-06 07:29:26 +0900

    Init new project, and start solving armstrong_number

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,2 @@
-use std::io;
-
-mod simple_array_sum;
-
-fn main() {
-    // variable declaration
-    let mut _num_str_1 = String::new();
-    let mut _num_str_2 = String::new();
-
-    // read variables
-    io::stdin().read_line(&mut _num_str_1).ok().expect("read error");
-    io::stdin().read_line(&mut _num_str_2).ok().expect("read error");
-
-    // parse integers
-    let mut _num_1 : i32 = _num_str_1.trim().parse().ok().expect("parse error");
-    let mut _num_2 : i32 = _num_str_2.trim().parse().ok().expect("parse error");
-
-    // print the sum
-    // Hint: Type println!("{}", _num_1 + _num_2); below 
-    println!("{}", _num_1 + _num_2);
-}
+mod armstrong_number;
+pub use armstrong_number::is_armstrong_number;
