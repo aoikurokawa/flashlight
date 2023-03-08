b68f16871b14586c07d34e779a57a5fb0a21e7a2
Aoi1011 <ht.so.ik0102@gmail.com>
2023-03-09 07:13:31 +0900

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod armstrong_number;
 mod gigasecond;
 mod isbn_verifier;
+mod robot_name;
 
 pub use armstrong_number::is_armstrong_number;
 pub use gigasecond::*;
 pub use isbn_verifier::is_valid_isbn;
+pub use robot_name::*;
---
+++ src/robot_name.rs
@@ -0,0 +1,49 @@
+use std::collections::HashSet;
+use std::sync::Mutex;
+
+use once_cell::sync::Lazy;
+use rand::Rng;
+
+const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const ROBOT_NAME_LEN: usize = 5;
+
+static SET: Lazy<Mutex<HashSet<Robot>>> = Lazy::new(|| Mutex::new(HashSet::new()));
+
+#[derive(Eq, PartialEq, Default, Hash)]
+pub struct Robot {
+    name: String,
+}
+
+impl Robot {
+    pub fn new() -> Self {
+        let mut robot = Robot::default();
+        robot.reset_name();
+        robot
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn reset_name(&mut self) {
+        loop {
+            let mut rng = rand::thread_rng();
+            let letter: String = (0..2)
+                .map(|_| {
+                    let idx = rng.gen_range(0..CHARSET.len());
+                    CHARSET[idx] as char
+                })
+                .collect();
+            let num1 = rng.gen_range(0..10);
+            let num2 = rng.gen_range(0..10);
+            let num3 = rng.gen_range(0..10);
+
+            let temp_name = format!("{}{}{}{}", letter, num1, num2, num3);
+
+            if SET.lock().unwrap().insert(Robot { name: temp_name.clone() }) {
+                self.name = temp_name;
+                break;
+            }
+        }
+    }
+}
