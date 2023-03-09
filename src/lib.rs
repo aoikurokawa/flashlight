099efa01154fe5570555a22ac4f45f08022a50a8
Aoi1011 <ht.so.ik0102@gmail.com>
2023-03-10 07:16:45 +0900

    Pass first test

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,10 @@ mod armstrong_number;
 mod gigasecond;
 mod isbn_verifier;
 mod robot_name;
+mod simple_cipher;
 
 pub use armstrong_number::is_armstrong_number;
 pub use gigasecond::*;
 pub use isbn_verifier::is_valid_isbn;
 pub use robot_name::*;
+pub use simple_cipher::*;
---
+++ src/simple_cipher.rs
@@ -0,0 +1,78 @@
+use rand::Rng;
+
+const ALPHABET: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
+
+pub fn encode(key: &str, s: &str) -> Option<String> {
+    let mut gaps = vec![];
+    let mut encoded = String::new();
+
+    for key_ch in key.as_bytes().iter() {
+        for (index, alpha) in ALPHABET.iter().enumerate() {
+            if alpha == key_ch {
+                gaps.push(index);
+            }
+        }
+    }
+
+    for (val_index, val_ch) in s.as_bytes().iter().enumerate() {
+        for (index, alpha) in ALPHABET.iter().enumerate() {
+            if alpha == val_ch {
+                encoded = format!("{}{}", encoded, ALPHABET[index + gaps[val_index]] as char);
+            }
+        }
+    }
+
+    Some(encoded)
+}
+
+pub fn decode(key: &str, s: &str) -> Option<String> {
+    let mut gaps = vec![];
+    let mut decoded = String::new();
+
+    for key_ch in key.as_bytes().iter() {
+        for (index, alpha) in ALPHABET.iter().enumerate() {
+            if alpha == key_ch {
+                gaps.push(index);
+            }
+        }
+    }
+
+    for (val_index, val_ch) in s.as_bytes().iter().enumerate() {
+        for (index, alpha) in ALPHABET.iter().enumerate() {
+            if alpha == val_ch {
+                decoded = format!("{}{}", decoded, ALPHABET[index - gaps[val_index]]);
+            }
+        }
+    }
+
+    Some(decoded)
+}
+
+pub fn encode_random(s: &str) -> (String, String) {
+    let s_len = s.len();
+    let mut rng = rand::thread_rng();
+
+    let random_str: String = (0..s_len)
+        .map(|_| {
+            let idx = rng.gen_range(0..ALPHABET.len());
+            ALPHABET[idx] as char
+        })
+        .collect();
+
+    let encoded = encode(random_str.as_str(), s).unwrap();
+
+    (random_str, encoded)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const PLAIN_TEXT: &str = "thisismysecret";
+    const KEY: &str = "abcdefghij";
+
+    #[test]
+    fn cipher_can_encode_with_given_key() {
+        assert_eq!(encode(KEY, "aaaaaaaaaa"), Some(KEY.to_string()));
+    }
+}
