b6fd9d0a946e05153dbd5d5dae6dcebdefdfe29b
Aoi1011 <ht.so.ik0102@gmail.com>
2023-03-14 06:30:46 +0900

    Implement sing function and add new some test

--- src/beer_song.rs
+++ src/beer_song.rs
@@ -15,7 +15,22 @@ pub fn verse(n: u32) -> String {
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    let str = String::new();
+    let mut str = String::new();
+    let mut count = start;
+
+    loop {
+        if count == end {
+            str = format!("{}{}", str, verse(count));
+        } else {
+            str = format!("{}{}\n", str, verse(count));
+        }
+
+        if count == end {
+            break;
+        }
+
+        count -= 1;
+    }
 
     str
 }
@@ -42,5 +57,14 @@ mod tests {
     #[test]
     fn test_verse_8() {
         assert_eq!(verse(8), "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n");
+    }
+
+    #[test]
+    fn test_song_8_6() {
+        assert_eq!(sing(8, 6), "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n");
+    }
+    #[test]
+    fn test_song_3_0() {
+        assert_eq!(sing(3, 0), "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n\n2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
     }
 }
