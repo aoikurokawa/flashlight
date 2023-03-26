70ee2dfdb65ef251f3f542d4326693bb3a156491
Aoi1011 <ht.so.ik0102@gmail.com>
2023-03-26 14:39:38 +0900

    Create mandel.png

--- src/bin/mandelbrot.rs
+++ src/bin/mandelbrot.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::{fs::File, str::FromStr};
 
 use image::codecs::png::PngEncoder;
@@ -38,6 +39,13 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
             (Ok(l), Ok(r)) => Some((l, r)),
             _ => None,
         },
+    }
+}
+
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None,
     }
 }
 
@@ -92,12 +100,32 @@ fn write_image(
     let output = File::create(filename)?;
 
     let encoder = PngEncoder::new(output);
-    match encoder.write_image(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::L8) {
-        Ok(_) => Ok(()),
-        Err(e) => Err(e.into()),
-    }
+    encoder
+        .write_image(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::L8)
+        .unwrap();
+
+    Ok(())
 }
 
 fn main() {
-    println!("Hello World");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!(
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        );
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
