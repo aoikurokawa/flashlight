93ae749750939735918a6768cde12833023f028b
Aoi1011 <ht.so.ik0102@gmail.com>
2023-03-29 06:55:20 +0900

    Fix

--- src/word_count.rs
+++ src/word_count.rs
@@ -9,7 +9,7 @@ pub fn word_count(words: &str) -> HashMap<String, u32> {
     for temp_word in words.split_whitespace().into_iter() {
         for word in temp_word.split(&[',', '\n', '\t']).collect::<Vec<&str>>() {
             let mut word = word.replace(&EXCLUDE_LIST[..], "");
-            if word != "" {
+            if !word.is_empty() {
                 let chars = word.chars().collect::<Vec<char>>();
                 if chars[0] == '\'' && chars[chars.len() - 1] == '\'' {
                     word = word.replace('\'', "");
