348e0b1e4eef670a0244d7062cae44fe814799b5
Aoi1011 <ht.so.ik0102@gmail.com>
2023-03-29 07:20:47 +0900

    Update

--- src/leap.rs
+++ src/leap.rs
@@ -1,6 +1,6 @@
 pub fn is_leap_year(year: u64) -> bool {
     if year % 4 == 0 && year % 100 != 0 {
-        return true;
+        true
     } else if year % 4 == 0 && year % 100 == 0 {
         if year % 400 == 0 {
             return true;
