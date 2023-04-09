c88ef38168afcadd6410eefd09950e708f9becfc
Aoi1011 <ht.so.ik0102@gmail.com>
2023-04-10 07:58:15 +0900

    Create new solution

--- src/acronym.rs
+++ src/acronym.rs
@@ -1,4 +1,4 @@
-pub fn abbreviate(phrase: &str) -> String {
+pub fn abbreviate1(phrase: &str) -> String {
     let mut acronym = String::new();
 
     for (_index, word) in phrase.split_whitespace().enumerate() {
@@ -32,6 +32,20 @@ pub fn abbreviate(phrase: &str) -> String {
     }
 
     acronym.to_uppercase()
+}
+
+pub fn abbreviate(phrase: &str) -> String {
+    phrase
+        .split(|c: char| c.is_ascii_whitespace() || c == '_' || c == '-')
+        .flat_map(|word| {
+            word.chars().take(1).chain(
+                word.chars()
+                    .skip_while(|c| c.is_ascii_uppercase())
+                    .filter(|c| c.is_ascii_uppercase()),
+            )
+        })
+        .collect::<String>()
+        .to_ascii_uppercase()
 }
 
 #[cfg(test)]
