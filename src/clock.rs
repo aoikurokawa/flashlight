dc1ee9b90af1c61d80d63cc115f977c909b541f9
Aoi1011 <ht.so.ik0102@gmail.com>
2023-04-09 12:14:22 +0900

    Update clock problem

--- src/clock.rs
+++ src/clock.rs
@@ -1,28 +1,28 @@
 use std::fmt::Display;
 
-const DAY: i32 = 24 * 60;
-const HOUR: i32 = 60;
-
 #[derive(Debug, PartialEq)]
 pub struct Clock {
+    hours: i32,
     minutes: i32,
 }
 
 impl Display for Clock {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:02}:{:02}", self.minutes / HOUR, self.minutes % HOUR)
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Clock {
-            minutes: (((hours * HOUR + minutes) % DAY) + DAY) % DAY,
+        let total_hours = (hours * 60 + minutes).rem_euclid(24 * 60);
+        Self {
+            hours: total_hours / 60,
+            minutes: total_hours % 60,
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Clock::new(0, self.minutes + minutes)
+        Clock::new(self.hours, self.minutes + minutes)
     }
 }
 
