38aaf9b88232359b3de27344b3273a4d4d1f4803
Aoi1011 <ht.so.ik0102@gmail.com>
2023-04-11 20:51:24 +0900

    Default function

--- src/bin/trait_treasure_box.rs
+++ src/bin/trait_treasure_box.rs
@@ -1,6 +1,9 @@
-trait TreasureBox {
-    fn open(&self, key_no: i32) -> bool;
+pub trait TreasureBox {
+    fn open(&self, key_no: i32) -> bool {
+        self.get_key_no() == key_no
+    }
     fn check(&self);
+    fn get_key_no(&self) -> i32;
 }
 
 struct JewelryBox {
@@ -9,28 +12,29 @@ struct JewelryBox {
 }
 
 impl TreasureBox for JewelryBox {
-    fn open(&self, key_no: i32) -> bool {
-        self.key_no == key_no
-    }
-
     fn check(&self) {
         println!("Got gold {}", self.price);
+    }
+
+    fn get_key_no(&self) -> i32 {
+        self.key_no
     }
 }
 
 struct TrapBox {
+    key_no: i32,
     damage: i32,
 }
 
 impl TreasureBox for TrapBox {
-    fn open(&self, _key_no: i32) -> bool {
-        return true;
+    fn check(&self) {
+        println!("It's trap, got damage {}", self.damage);
     }
 
-    fn check(&self) {
-         println!("It's trap, got damage {}", self.damage);
+    fn get_key_no(&self) -> i32 {
+        self.key_no
     }
- }
+}
 
 fn open_box(tbox: impl TreasureBox, key_no: i32) {
     if !tbox.open(key_no) {
@@ -41,9 +45,18 @@ fn open_box(tbox: impl TreasureBox, key_no: i32) {
 }
 
 fn main() {
-    let box1 = JewelryBox {price: 20, key_no: 1};
-    let box2 = TrapBox {damage: 3};
-    let box3 = JewelryBox {price: 20, key_no: 2};
+    let box1 = JewelryBox {
+        price: 20,
+        key_no: 1,
+    };
+    let box2 = TrapBox {
+        damage: 3,
+        key_no: 3,
+    };
+    let box3 = JewelryBox {
+        price: 20,
+        key_no: 2,
+    };
 
     let my_key = 2;
     open_box(box1, my_key);
