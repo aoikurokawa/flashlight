a662ee0935872c9eaf71a13c4d9904dba00f65a5
Aoi1011 <ht.so.ik0102@gmail.com>
2023-04-28 06:04:26 +0900

    refact: update solution, does not use binary_search method

--- src/binary_search.rs
+++ src/binary_search.rs
@@ -1,7 +1,12 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    match array.binary_search(&key) {
-        Ok(index) => Some(index),
-        Err(_) => None,
+use std::cmp::Ordering;
+
+pub fn find<R: AsRef<[T]>, T: Ord>(array: R, key: T) -> Option<usize> {
+    let array = array.as_ref();
+    let mid = array.len() / 2;
+    match key.cmp(array.get(mid)?) {
+        Ordering::Equal => Some(mid),
+        Ordering::Less => find(&array[..mid], key),
+        Ordering::Greater => find(&array[mid + 1..], key).map(|i| i + mid + 1),
     }
 }
 
