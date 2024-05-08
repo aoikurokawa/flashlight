5211b90b5e9ad074970c476e00a3b8199e7528d5
aoikurokawa <ht.so.ik0102@gmail.com>
2024-05-08 21:33:44 +0900

    fix: read private key from .env for now

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use drift_sdk::{
 };
 use flashlight::jit_maker::{JitMaker, JitMakerConfig};
 use rust::{jitter::Jitter, types::ComputeBudgetParams};
-use solana_sdk::{commitment_config::CommitmentConfig, signature::read_keypair_file};
+use solana_sdk::commitment_config::CommitmentConfig;
 use tokio::sync::Mutex;
 
 #[derive(Parser)]
@@ -27,7 +27,7 @@ struct Cli {
 enum Commands {
     /// Initialize user
     InitUser {},
-    Run {},
+    RunJit {},
 }
 
 #[tokio::main]
@@ -36,20 +36,21 @@ async fn main() {
     dotenv().ok();
     env_logger::init();
 
-    let endpoint = env::var("RPC_URL").expect("DATABASE_URL must be set");
+    let endpoint = env::var("RPC_URL").expect("RPC_URL must be set");
     let private_key = env::var("PRIVATE_KEY").expect("SECRET_KEY must be set");
     let wallet =
         drift_sdk::Wallet::new(load_keypair_multi_format(&private_key).expect("valid keypair"));
 
     match cli.command {
         Commands::InitUser {} => {
-            let keypair = read_keypair_file(&private_key).expect("read keypair");
-            let secret_key_slices = keypair.secret().to_bytes();
-            let key = String::from_utf8(secret_key_slices.to_vec()).unwrap();
-            println!("{:?}", key);
-            
+            // let keypair = read_keypair_file(&private_key).expect("read keypair");
+            // let secret_key_slices = keypair.secret().to_bytes();
+            // let key = bs58::encode(keypair.secret()).into_string();
+            // let key = String::from_utf8(secret_key_slices.to_vec()).unwrap();
+
+            println!("{:?}", wallet.signer());
         }
-        Commands::Run {} => {
+        Commands::RunJit {} => {
             let sub_accounts = vec![0];
 
             let mut drift_client = DriftClient::new(
