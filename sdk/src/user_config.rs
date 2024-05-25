8ddbd033fb6a473b9cdf171c4bb0b44166418104
aoikurokawa <ht.so.ik0102@gmail.com>
2024-05-25 10:00:51 +0900

    feat: user_config

--- sdk/src/user_config.rs
+++ sdk/src/user_config.rs
@@ -1,16 +1,25 @@
-use solana_sdk::commitment_config::CommitmentLevel;
+use solana_sdk::{commitment_config::CommitmentLevel, pubkey::Pubkey};
 
-use crate::accounts::BulkAccountLoader;
+use crate::{accounts::BulkAccountLoader, AccountProvider, DriftClient};
 
-pub enum UserSubscriptionConfig<T> {
+pub struct UserConfig<T, F, Fut, G, C>
+where
+    C: AccountProvider,
+{
+    account_subscription: Option<UserSubscriptionConfig<T, F, Fut, G>>,
+    drift_client: DriftClient<C>,
+    user_account_public_key: Pubkey,
+}
+
+pub enum UserSubscriptionConfig<T, F, Fut, G> {
     WebSocket {
         resub_timeout_ms: u16,
         log_resub_messages: bool,
         commitment: CommitmentLevel,
     },
-    // Polling {
-    //     account_loader: BulkAccountLoader,
-    // },
+    Polling {
+        account_loader: BulkAccountLoader<F, Fut, G>,
+    },
     Custom {
         user_account_subscriber: Box<T>,
     },
