4bb7415a3c2e9921bb15acbaa55fe97daa7ecc2f
aoikurokawa <ht.so.ik0102@gmail.com>
2024-06-01 11:38:26 +0900

    feat: perp markets

---
+++ sdk/src/config.rs
@@ -0,0 +1,4 @@
+pub enum DriftEnv {
+    Devnet,
+    MainnetBeta,
+}
--- sdk/src/priority_fee/mod.rs
+++ sdk/src/priority_fee/mod.rs
@@ -1,8 +1,8 @@
 mod average_over_slots_strategy;
-mod drift_priority_fee_method;
+pub mod drift_priority_fee_method;
 mod helius_priority_fee_method;
 mod max_over_slots_strategy;
 pub mod priority_fee_subscriber;
 pub mod priority_fee_subscriber_map;
 mod solana_priority_fee_method;
-mod types;
+pub mod types;
